aa91dc641dd8a87ba2249cf22d0203bbcaf35206
VARSHAC06 <varshacbca24@rvu.edu.in>
2025-01-29 12:05:06 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,19 +1,12 @@
-def find_largest_and_smallest(arr):
-    if not arr:
-       return None, None # return none if the array is empty
-    largest=arr[0]
-    smallest=arr[0]
-    for num in arr:
-       if num>largest:
-          largest=num
-       if num<smallest:
-          smallest=num
-    return largest, smallest
-     
-      #example usage:
 array=[3,5,1,8,-3,7,2]
-largest,smallest=find_largest_and_smallest(array)
-print("largest element:",largest)
-print("smallest element:",smallest)
-           
+
+max=array[0]
+min=array[0]
+for num in array:
+    if num>max:
+        max=num
+    if num<min:
+        min=num
+print(f"largest element: {max}")
+print(f"smallest element: {min}")
            
